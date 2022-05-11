b4c98412ab6c885661d9ad663b26d47db1a67d66
trungtran1211 <56382373+trungtran1211@users.noreply.github.com>
2022-05-11 13:45:45 +0700

    update package

--- src/Auth/RegristerForm/RegristerForm.jsx
+++ src/Auth/RegristerForm/RegristerForm.jsx
@@ -1,9 +1,19 @@
+import { Typography } from '@material-ui/core';
+import { LockOpenOutlined } from '@material-ui/icons';
+import { Avatar } from '@mui/material';
 import React from 'react';
 
 const RegristerForm = () => {
     return (
         <div>
+            <Avatar>
+                <LockOpenOutlined/>
+            </Avatar>
+            <Typography component="h3" variant="h5">
+                Create an Account
+            </Typography>
             
+
         </div>
     );
 };
