50cf83c1ec2524aea5286e792b87e244ea5f64d4
trungtran1211 <thanhtrungtran8888@gmail.com>
2023-06-06 17:43:27 +0700

    s

--- src/Container/Products/ProductDetails.jsx
+++ src/Container/Products/ProductDetails.jsx
@@ -17,6 +17,7 @@ function ProductDetails() {
             console.log(e.response);
         });
     },[id]);
+    console.log(product);
     return (
         <div className='container main'>
             test
